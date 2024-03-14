f7a5a7b2f725471b9d1f037aa72072c15b0bad64
CuteProgrammerSonya <145923312+CuteProgrammerSonya@users.noreply.github.com>
2024-03-14 17:13:07 +0300

    Update Object.h

--- Object.h
+++ Object.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "Position.h"
+#include <iostream>
+#include <chrono>
+
 class Object
 {
 protected:
 	int age;
 public:
 	Position pos;
-	obj();
+	Object();
 	virtual int act(Position*, Position*, Position*, Position*) { return 0; };
 	void spawn(Position* p);
 	void normalizepos(Position& p);
