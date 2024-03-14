0e5087748d46a501b63eba4422b0628438051fe7
CuteProgrammerSonya <145923312+CuteProgrammerSonya@users.noreply.github.com>
2024-03-14 17:14:14 +0300

    Update Predator.cpp

--- Predator.cpp
+++ Predator.cpp
@@ -1,30 +1,32 @@
 #include "Predator.h"
+const int pred_age = 100;
+const int pred_starving = 30;
 
 
-Predator::Predator(int x, int y, int st, int sp, int radius)
-{
-	Animal(sp, radius, st);
-	pos.x = x;
-	pos.y = y;
-}
+//Predator::Predator(int x, int y, int st, int sp, int radius)
+//{
+//	Animal(sp, radius, st);
+//	pos.x = x;
+//	pos.y = y;
+//}
 
 int Predator::act(Position* p, Position* eat, Position* sex, Position*)
 {
 	age++;
 	starve -= 2;
-	if (starve <= 0 || age > animal2age) return -1;
-		if (starve <= animal2starving && eat->x != -1 && eat->y != -1) {
-			pos.x += ((pos.x <= eat->x) * 2 - 1)*speed;
-			pos.y += ((pos.y <= eat->y) * 2 - 1)*speed;
-		}
-		else if (sex->x != -1 && sex->y != -1) {
-			pos.x += ((pos.x <= sex->x) * 2 - 1)*speed;
-			pos.y += ((pos.y <= sex->y) * 2 - 1)*speed;
-		}
-		else {
-			srand(clock());
-			
-		}
-		normalizepos();
+	if (starve <= 0 || age > pred_age) return -1;
+	if (starve <= pred_starving && eat->x != -1 && eat->y != -1) {
+		pos.x += ((pos.x <= eat->x) * 2 - 1) * speed;
+		pos.y += ((pos.y <= eat->y) * 2 - 1) * speed;
+	}
+	else if (sex->x != -1 && sex->y != -1) {
+		pos.x += ((pos.x <= sex->x) * 2 - 1) * speed;
+		pos.y += ((pos.y <= sex->y) * 2 - 1) * speed;
+	}
+	else {
+		srand(clock());
+
+	}
+	/*normalizepos();*/
 	return 0;
 }
