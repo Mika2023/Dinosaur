fd042c13082d0c2bc0b51646c0d2d4e1e2014331
CuteProgrammerSonya <145923312+CuteProgrammerSonya@users.noreply.github.com>
2024-03-14 17:00:56 +0300

    Update Animal.cpp

--- Animal.cpp
+++ Animal.cpp
@@ -14,7 +14,7 @@ Position Animal::check_vision(vector<Object> food)
 	for (int i = 0; i < size; ++i)
 	{
 		if (abs(food[i].pos.x - pos.x) <= vision_rad && abs(food[i].pos.y - pos.y) <= vision_rad)
-			return grass[i].pos;
+			return food[i].pos;
 	}
 	Position p;
 	p.x = -1;
