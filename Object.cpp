ca9168e7ad0694a5b455fb407d46114dc942d3e8
CuteProgrammerSonya <145923312+CuteProgrammerSonya@users.noreply.github.com>
2024-03-14 20:32:41 +0300

    Update Object.cpp

--- Object.cpp
+++ Object.cpp
@@ -1,6 +1,8 @@
 #include "Object.h"
+const size_t width = 20;
+const size_t height = 20;
 
-Object::obj()
+Object::Object()
 {
 	pos.x = -1;
 	pos.y = -1;
@@ -11,9 +13,10 @@ void Object::spawn(Position* p)
 {
 	do {
 		srand(clock()); //to avoid stacks of content. Not nessessary
-		
-		p->x = pos.x + rand() % 3 - 1;
-		p->y = pos.y + rand() % 3 - 1;
+		int x = pos.x + rand() % 3 - 1;
+		int y = pos.y + rand() % 3 - 1;
+		p->x = x;
+		p->y = y;
 		normalizepos(*p);
 		//make the good generation of position with checking destination cell if it occupied
 	} while (pos.y == p->y && pos.x == p->x);
