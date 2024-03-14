79750acc19a425c05fbd34efe85686f497c7727e
CuteProgrammerSonya <145923312+CuteProgrammerSonya@users.noreply.github.com>
2024-03-14 20:30:38 +0300

    Update Grass.cpp

--- Grass.cpp
+++ Grass.cpp
@@ -1,7 +1,7 @@
 #include "Grass.h"
-const int grass_age = 20;
-const int grass_spawnrate = 10; //lower is more
-Grass::food(int x, int y)
+const int grass_age = 10;
+const int grass_spawnrate = 5; //lower is more
+Grass::Grass(int x, int y)
 {
 	pos.x = x;
 	pos.y = y;
@@ -10,9 +10,12 @@ Grass::food(int x, int y)
 int Grass::act(Position* p, Position*, Position*, Position*)
 {//-1 delete obj; 1 create new obj; 0 do nothing. Have no aims
 	age++;
-	if (age > grass_age) return -1;//âñå ýòè ãëîáàëüíûå ïåðåìåííûå, êîòîðûå ìû ïåðåäàåì â ñàìîì íà÷àëå
-	//íàäî çàïèõàòü ëèáî â ïîëÿ ñîîòâåòñòâóþùèõ êëàññîâ, ëèáî ïåðåäàâàòü â ìåòîäû â êà÷åñòâå ïàðàìåòðîâ
-	if (age % grass_spawnrate == 0) {
+	if (age > grass_age)
+	{
+		return -1;
+	}
+	if (age % grass_spawnrate == 0) 
+	{
 		spawn(p);
 		return 1;
 	}
