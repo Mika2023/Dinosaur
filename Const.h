49879d0e7f8c0efcd24e5fd5c37e3d9bf716f5c2
CuteProgrammerSonya <145923312+CuteProgrammerSonya@users.noreply.github.com>
2024-03-22 22:33:03 +0300

    Update Const.h
    I add some necessary settings

--- Const.h
+++ Const.h
@@ -1,4 +1,10 @@
 #pragma once
+
+#define GREEN "\033[32m"
+#define ORANGE "\033[33m"
+#define BLUE "\033[36m"
+#define RESET "\033[0m"
+
 const int start_grass_count = 10;
 const int start_herb_count = 4;
 const int start_pred_count = 2;
@@ -13,6 +19,10 @@ const int herb_age = 40;
 const int herb_speed = 2;
 const int herb_starving = 10;
 const int herb_startstarve = 20;
+const int herb_rad = 4;
 
 const int pred_age = 100;
+const int pred_speed = 3;
 const int pred_starving = 30;
+const int pred_startstarve = 50;
+const int pred_rad = 6;
