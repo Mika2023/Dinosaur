a613922f764e59f5108bedef148a9f676e76c24c
CuteProgrammerSonya <145923312+CuteProgrammerSonya@users.noreply.github.com>
2024-03-22 22:57:13 +0300

    Update Predator.h
    Some minor changes in the heading

--- Predator.h
+++ Predator.h
@@ -1,14 +1,10 @@
 #pragma once
 #include "Animal.h"
 #include "Object.h"
+#include "Const.h"
+
 class Predator : public Animal
 {
-//public:
-//	static int get_count();
-//	Predator reproduce(const Predator& animal) const;
-//	void eat_herb() const;
-//protected:
-//	static int count;
 public:
 	Predator(int x, int y, int st, int sp, int radius);
 	int act(Position* p, Position* eat, Position* sex, Position*) override;
