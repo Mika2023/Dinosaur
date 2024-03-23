d31b29dff969f9d8a15e42d1b3b77e635d8658e2
CuteProgrammerSonya <145923312+CuteProgrammerSonya@users.noreply.github.com>
2024-03-24 00:26:24 +0300

    Update Animal.h
    We add pattern check_vision

--- Animal.h
+++ Animal.h
@@ -12,7 +12,22 @@ protected:
 	int starve;
 	int vision_rad;
 public:
-        Animal();
+        //Animal();
 	Animal(int sp, int radius, int st);
-	Position check_vision(vector<Object> foods);
+        template<typename Type>
+	Position check_vision(vector<Type>& food)
+	{
+		int size = food.size();
+		for (int i = 0; i < size; ++i)
+		{
+			if (abs(((Object)food[i]).pos.x - pos.x) <= vision_rad && abs(((Object)food[i]).pos.y - pos.y) <= vision_rad)
+				return ((Object)food[i]).pos;
+			/*if (abs(food[i].pos.x - pos.x) <= vision_rad && abs(food[i].pos.y - pos.y) <= vision_rad)
+				return food[i].pos;*/
+		}
+		Position p;
+		p.x = -1;
+		p.y = -1;
+		return p;
+	}
 };
