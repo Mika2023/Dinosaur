15ffd1098eb1db1fb831e259655e11e4eca5df54
CuteProgrammerSonya <145923312+CuteProgrammerSonya@users.noreply.github.com>
2024-03-24 00:17:58 +0300

    Update Animal.cpp
    Now this is the necessary constructor

--- Animal.cpp
+++ Animal.cpp
@@ -1,11 +1,11 @@
 #include "Animal.h"
 
-Animal::Animal()
-{
-	starve = 0;
-	speed = 0;
-	vision_rad = 0;
-}
+//Animal::Animal()
+//{
+//	starve = 0;
+//	speed = 0;
+//	vision_rad = 0;
+//}
 
 Animal::Animal(int sp, int radius, int st)
 {
@@ -15,17 +15,17 @@ Animal::Animal(int sp, int radius, int st)
 	starve = st;
 }
 
-Position Animal::check_vision(vector<Object> food)
-{
-	int size = food.size();
-	for (int i = 0; i < size; ++i)
-	{
-		if (abs(food[i].pos.x - pos.x) <= vision_rad && abs(food[i].pos.y - pos.y) <= vision_rad)
-			return food[i].pos;
-	}
-	Position p;
-	p.x = -1;
-	p.y = -1;
-	return p;
-}
+//Position Animal::check_vision(vector<Object> food)
+//{
+//	int size = food.size();
+//	for (int i = 0; i < size; ++i)
+//	{
+//		if (abs(food[i].pos.x - pos.x) <= vision_rad && abs(food[i].pos.y - pos.y) <= vision_rad)
+//			return food[i].pos;
+//	}
+//	Position p;
+//	p.x = -1;
+//	p.y = -1;
+//	return p;
+//}
 
