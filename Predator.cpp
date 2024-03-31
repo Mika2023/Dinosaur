c873046a5743b8e3f500b941f5e3de73f653fc77
vituhaa <145931387+vituhaa@users.noreply.github.com>
2024-03-31 23:09:31 +0300

    Predator.cpp

--- Predator.cpp
+++ Predator.cpp
@@ -11,20 +11,25 @@ Predator::Predator(int x, int y, int st, int sp, int radius): Animal(sp, radius,
 int Predator::act(Position* p, Position* eat, Position* sex, Position*)
 {
 	age++;
-	starve -= 2;
-	if (starve <= 0 || age > pred_age) return -1;
-	if (starve <= pred_starving && eat->x != -1 && eat->y != -1) {
-		pos.x += ((pos.x <= eat->x) * 2 - 1) * speed;
-		pos.y += ((pos.y <= eat->y) * 2 - 1) * speed;
-	}
-	else if (sex->x != -1 && sex->y != -1) {
-		pos.x += ((pos.x <= sex->x) * 2 - 1) * speed;
-		pos.y += ((pos.y <= sex->y) * 2 - 1) * speed;
-	}
-	else {
-		srand(clock());
-
-	}
-	/*normalizepos();*/
-	return 0;
+        starve -= 2;
+        if (starve <= 0 || age > pred_age)
+            return -1;
+        if (starve <= pred_starving && eat->x != -1 && eat->y != -1)
+        {
+            pos.x += ((pos.x <= eat->x) * 2 - 1) * speed;
+            pos.y += ((pos.y <= eat->y) * 2 - 1) * speed;
+        }
+        else if (sex->x != -1 && sex->y != -1)
+        {
+            pos.x += ((pos.x <= sex->x) * 2 - 1) * speed;
+            pos.y += ((pos.y <= sex->y) * 2 - 1) * speed;
+        }
+        else
+        {
+            srand(clock());
+        }
+        normalizepos(pos);
+        p->x = pos.x;
+        p->y = pos.y;
+        return 1;
 }
